5dc34672c2071fe6fdc19a934cfd6bbc099dd2cf
sceletoniK <L.s1n56@yandex.ru>
2021-07-06 14:52:21 +0300

    Добавьте небольшой патч дизайна

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -49,17 +49,17 @@ namespace Team_Two_Work
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Location = new System.Drawing.Point(45, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(372, 20);
+            this.label1.Size = new System.Drawing.Size(332, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Сейчас мы попытаемся отгадать твой возраст!";
+            this.label1.Text = "Сейчас мы попытаемся отгадать возраст!";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(115, 120);
+            this.label2.Location = new System.Drawing.Point(112, 120);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 24);
             this.label2.TabIndex = 3;
@@ -68,7 +68,7 @@ namespace Team_Two_Work
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(99, 161);
+            this.label3.Location = new System.Drawing.Point(97, 161);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 4;
